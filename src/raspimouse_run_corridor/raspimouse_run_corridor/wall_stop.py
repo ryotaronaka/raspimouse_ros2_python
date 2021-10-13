6f32aa30f66db769fa0166339394bef3cb4734ea
Ubuntu <34445043+ryotaronaka@users.noreply.github.com>
2021-10-13 05:07:24 +0000

    Add raspimouse_run_corridor pkg and wall_stop node

---
+++ src/raspimouse_run_corridor/raspimouse_run_corridor/wall_stop.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python
+
+# colcon build
+# source ~/ros2_ws/install/setup.bash
+
+# ros2 run raspimouse_run_corridor wall_stop
+
+import rclpy,copy
+from geometry_msgs.msg import Twist
+from raspimouse_msgs.action import Movement
+from std_srvs.srv import Trigger
+from raspimouse_msgs.msg import LightSensors
+from rclpy.node import Node
+import time
+
+class WallStop(Node):
+    def __init__(self):
+        super().__init__('wall_stop')
+        # コンストラクター定義は，service nodeと同じタイプと名前のclient nodeを作成する．
+        # タイプと名前は，clientとserviceが通信できるように一致する必要がある．
+        self.cli = self.create_client(Trigger, '/motor_on')
+        
+        # clientのタイプと名前に一致するserviceが利用可能かどうか，1秒に1回チェック．
+        while not self.cli.wait_for_service(timeout_sec=1.0):
+            self.get_logger().info('/motor_on service not available, waiting again...')
+        
+        # リクエスト定義
+        self.sensor_values = LightSensors()
+        self.subscription = self.create_subscription(
+            LightSensors, # Int16MultiArray,
+            'lightsensors',
+            self.callback,
+            10
+        )
+
+        self.publisher_ = self.create_publisher( 
+            Twist,
+            '/cmd_vel',
+            10
+        )
+
+        timer_period = 1.0
+        self.timer = self.create_timer(timer_period, self.run_callback)
+    
+    def callback(self, msg):
+        self.sensor_values = msg
+        #self.get_logger().info('I heard right_forward : "%d"' % msg.right_forward)
+        #self.get_logger().info('I heard right_side : "%d"' % msg.right_side)
+        #self.get_logger().info('I heard left_side : "%d"' % msg.left_side)
+        #self.get_logger().info('I heard left_forward : "%d"' % msg.left_forward)
+    
+    def run_callback(self):
+        data = Twist()
+        data.linear.x = 0.2 if self.sensor_values.sum_all < 500 else 0.0
+        self.publisher_.publish(data)
+
+
+def main(args=None):
+    rclpy.init(args=args)
+    wall_stop = WallStop()
+    rclpy.spin(wall_stop)
+    wall_stop.destroy_node()
+    rclpy.shutdown()
+
+
+if __name__ == '__main__':
+    main()
